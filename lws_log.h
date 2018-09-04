71376523c726ee13cb4530578127240b676f3386
hancq <cq.han@meari.com.cn>
2018-09-04 10:07:28 +0800

    add log interfaces

--- lws_log.h
+++ lws_log.h
@@ -24,6 +24,9 @@ extern log_level_t lws_get_log_level(void);
 extern int lws_logger(log_level_t level, const char *filename, int line, const char *format, ...);
 
 #define lws_log(level, ...)   lws_logger(level, __FILE__, __LINE__, ##__VA_ARGS__)
+#define lws_err(...)    lws_logger(LOG_LEVEL_ERR, __FILE__, __LINE__, ##__VA_ARGS__)
+#define lws_wrn(...)    lws_logger(LOG_LEVEL_WARN, __FILE__, __LINE__, ##__VA_ARGS__)
+#define lws_dbg(...)    lws_logger(LOG_LEVEL_INFO, __FILE__, __LINE__, ##__VA_ARGS__)
 
 #endif // _LWS_LOG_H_
 
