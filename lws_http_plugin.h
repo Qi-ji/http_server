d88306a6b7213d8cfe5175ed31ebfe278102aae1
hancq <cq.han@meari.com.cn>
2018-09-19 10:41:42 +0800

    delete old files

--- lws_http_plugin.h
+++
@@ -1,9 +0,0 @@
-#ifndef _LWS_HTTP_PLUGIN_H_
-#define _LWS_HTTP_PLUGIN_H_
-
-extern void lws_default_handler(lws_http_conn_t *c, int ev, void *p);
-extern void lws_hello_handler(lws_http_conn_t *c, int ev, void *p);
-extern void lws_version_handler(lws_http_conn_t *c, int ev, void *p);
-
-#endif // _LWS_HTTP_PLUGIN_H_
-
